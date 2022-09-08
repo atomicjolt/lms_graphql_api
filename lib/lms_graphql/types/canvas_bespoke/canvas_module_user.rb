072f113cd99703ecf8ee6279fa7ed0bda76feef5
Justin Ball <justinball@gmail.com>
2022-09-08 17:29:14 -0600

    Remove unused field

--- lib/lms_graphql/types/canvas_bespoke/canvas_module_user.rb
+++ lib/lms_graphql/types/canvas_bespoke/canvas_module_user.rb
@@ -9,7 +9,6 @@ module LMSGraphQL
       class CanvasModuleUser < LMSGraphQL::Types::Canvas::BaseType
         description "A user specific to data returned from bulk_user_progress"
         field :id, ID, "The ID of the user..Example: 2", null: false
-        field :name, String, "The name of the user..Example: Sheldon Cooper", null: true
         field :display_name, String, "The name of the user that is should be used for sorting groups of users, such as in the gradebook..Example: Cooper, Sheldon", null: true
         field :avatar_image_url, String, "If avatars are enabled, this field will be included and contain a url to retrieve the user's avatar..Example: https://en.gravatar.com/avatar/d8cb8c8cd40ddf0cd05241443a591868?s=80&r=g", null: true
         field :html_url, String, "", null: true
